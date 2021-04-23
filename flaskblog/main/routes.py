4f5d3e525b577503103be9dab7acae182bf1227c
sanjuGoudru <sanjugr7022@gmail.com>
2021-04-23 13:41:00 +0530

    Correct title for Home and About page

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -19,8 +19,8 @@ main = Blueprint('main',__name__)
 def home():
     page = request.args.get('page',1,type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page,per_page=5)
-    return render_template('home.html',title='Ma sguy',posts=posts)
+    return render_template('home.html',title='My Blog',posts=posts)
 
 @main.route('/about')
 def about():
-    return render_template('about.html',title='Ma sguy')
+    return render_template('about.html',title='About')
